ff98568ca436267d31b9707f425c371427e5bf94
alexandr <assemblertai@gmail.com>
2015-11-05 19:45:08 +0200

    add label

--- part2/HelloWorldWindow.java
+++ part2/HelloWorldWindow.java
@@ -1,11 +1,12 @@
-import javax.swing.JFrame;
+import javax.swing.*;
 
 class HelloWorldWindow
 {
     public static void main(String args[])
     {
 	JFrame frame = new JFrame("Hello");
-
+	JLabel label = new JLabel(" Hello Java", JLabel.CENTER);
+        frame.add(label);	
 	frame.setSize(300, 300);
 	frame.setVisible(true);
     }
