45427fdf291fc34c6beab9cd453117ddb3645c46
alexandr <assemblertai@gmail.com>
2015-11-05 19:08:26 +0200

    add window prog

---
+++ part2/HelloWorldWindow.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+
+class HelloWorldWindow
+{
+    public static void main(String args[])
+    {
+	JFrame frame = new JFrame("Hello");
+
+	frame.setSize(300, 300);
+	frame.setVisible(true);
+    }
+}
